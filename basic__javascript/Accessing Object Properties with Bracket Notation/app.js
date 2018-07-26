3466c3b541c7faaaaabe0fe68b79a2f2971b9d31
Jeff Oliver <jeffoliver3384@gmail.com>
2018-07-26 19:30:06 -0400

    Accessing Object Properties with Bracket Notation

--- basic__javascript/Accessing Object Properties with Bracket Notation/app.js
+++ basic__javascript/Accessing Object Properties with Bracket Notation/app.js
@@ -9,4 +9,4 @@ var testObj = {
 //Only change code below this line
 
 var entreeValue = testObj['an entree']; //Change this line
-var drinkValue = testObj;['the drink'] //Change this line
+var drinkValue = testObj['the drink']; //Change this line
