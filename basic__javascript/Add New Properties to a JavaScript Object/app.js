c06643c9d79190a57432772c13428e5d6634c60b
Jeff Oliver <jeffoliver3384@gmail.com>
2018-07-29 22:21:24 -0400

    Add New Properties to a JavaScript Object

---
+++ basic__javascript/Add New Properties to a JavaScript Object/app.js
@@ -0,0 +1,25 @@
+// Example
+
+// var ourDog = {
+//   name: 'Camper',
+//   legs: 4,
+//   tails: 1,
+//   friends: ['Everything!']  
+// };
+
+// ourDog.bark = 'Bow-Wow';
+// console.log(ourDog);
+
+// Setup
+
+var myDog = {
+  name: 'Happy Coder',
+  legs: 4,
+  tails: 1,
+  friends: ['Everything!']
+};
+
+//Only change code below this line
+
+myDog.bark = 'woof';
+console.log(myDog);
