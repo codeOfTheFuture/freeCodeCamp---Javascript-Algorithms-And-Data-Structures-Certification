c00f74051721dfbaff3d34435a62a7daccc19a6c
Jeff Oliver <jeffoliver3384@gmail.com>
2018-07-29 22:11:09 -0400

    Updating Object Properties

---
+++ basic__javascript/Updating Object Properties/app.js
@@ -0,0 +1,25 @@
+// // Example
+
+// var ourDog = {
+//   name: 'Camper',
+//   legs: 4,
+//   tails: 1,
+//   friends: ['Everything!']
+// };
+
+// ourDog.name = 'Happy Camper';
+// console.log(ourDog);
+
+// Setup
+
+var myDog = {
+  name: 'Coder',
+  legs: 4,
+  tails: 1,
+  friends: ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+
+myDog['name'] = 'Happy Coder';
+console.log(myDog['name']);
