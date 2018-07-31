0fe41b1d263afc32d36a0e035cce5654accb8568
Jeff Oliver <jeffoliver3384@gmail.com>
2018-07-30 23:41:30 -0400

    Delete Properties from a JavaScript Object

---
+++ basic__javascript/Delete Properties from a JavaScript Object/app.js
@@ -0,0 +1,15 @@
+// Example
+
+var ourDog = {
+  name: 'Camper',
+  legs: 4,
+  tails: 1,
+  friends: ['everything!'],
+  bark: 'bow-wow'
+};
+
+console.log(ourDog);
+
+delete ourDog.bark;
+
+console.log(ourDog);
