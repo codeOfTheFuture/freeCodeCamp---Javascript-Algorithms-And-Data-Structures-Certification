16ab0ad242eaf0405afcb17856c97a81cc3c3740
Jeff Oliver <jeffoliver3384@gmail.com>
2018-08-21 01:43:28 -0400

    Manipulating Complex Objects

---
+++ basic__javascript/Manipulating Complex Objects/app.js
@@ -0,0 +1,26 @@
+var myMusic = [
+  {
+    'artist': 'Billy Joel',
+    'title': 'Piano Man',
+    'release_year': 1973,
+    'formats': [
+      'CD',
+      '8T',
+      'LP'
+    ],
+    'gold': true
+  },
+  //Add record here
+
+  {
+    'artist': 'Tool',
+    'title': 'AEnima',
+    'release_year': 1996,
+    'formats': [
+      'CD',
+      'Cassette',
+      'LP'
+    ],
+    'platinum': true
+  }
+];
